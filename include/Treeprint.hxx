06cba71b5f0b31349cf6fc0e92fdcf71c88b83e6
Hossain Md. Fahim <hossainmdfahim66@gmail.com>
2026-03-30 03:19:22 +0600

    feat: Add modifyGrid()

--- include/Treeprint.hxx
+++ include/Treeprint.hxx
@@ -9,8 +9,10 @@
 #endif
 
 #include <concepts>
+#include <cstddef>
 #include <string>
 #include <type_traits>
+#include <unordered_set>
 #include <vector>
 
 struct Treenode
@@ -37,7 +39,6 @@ struct Cell
 {
   std::string val_{};
   bool changed_{false};
-
   Cell(const std::string& s, bool c)
     : val_{s},
       changed_{c} {};
@@ -88,9 +89,71 @@ private:
       return std::to_string(val);
   }
 
+  void modifyGrid(size_t row, size_t col)
+  {
+    if (mRoot == nullptr)
+      return;
+    
+    std::vector<Cell> vec{col + 1, Cell{"", false}};
+    mGrid.push_back(vec);
+    
+    if (mVisited.find(mRoot) != mVisited.end())
+      mGrid[row][col].val_ = "❌️";
+    else
+      mGrid[row][col].val_ = std::to_string(mPtr);
+    mGrid[row][col].changed_ = true;
+    mUsedRow.insert(row);
+    
+    if (col > 1)
+    {
+      mGrid[row][col-1].val_ = "─";
+      mGrid[row][col-1].changed_ = true;
+    }
+    
+    size_t i{row - 1};
+    if(col >= 2)
+    {
+      mGrid[row][col - 2].val_ = "├─";
+      mGrid[row][col - 2].changed_ = true;
+      while (i > 0 and !mGrid[i][col - 2].changed_)
+      {
+        mGrid[i][col - 2].val_ = "│";
+        mGrid[i][col - 2].changed_ = true;
+        --i;
+      }
+    }
+    
+    size_t next_row{row + 1};
+    size_t next_col{col + 2};
+
+    while (mUsedRow.find(next_row) != mUsedRow.end())
+    {
+      ++next_row;
+    }
+
+    std::vector<Treenode*> children{mRoot->get_children()};
+    if (mVisited.find(mRoot) != mVisited.end())
+    {
+      children.clear();
+    }
+    mVisited.insert(mRoot);
+
+    for (Treenode* child : children)
+    {
+      modifyGrid(next_row, next_col, child, mGrid, mVisited);
+      while (mUsedRow.find(next_row) != mUsedRow.end())
+      {
+        ++next_row;
+      }
+    }
+  }
+
 private:
   Treenode* mRoot{nullptr};
   Tagtype Nodetype::* mPtr{nullptr};
+  std::vector<std::vector<Cell>> mGrid{};
+  std::unordered_set<Nodetype*> mVisited{};
+  std::unordered_set<size_t> mUsedRow{};
 
 private:
   TREEPRINT_FRIEND_TESTS
