67f2a3ccc1ec88d327d5d445a81c96fdc9920b80
Hossain Muhammad Fardin <hmfardin58@gmail.com>
2026-04-11 12:49:57 +0000

    feat: Add color to path rendering

--- drafts/test2.cxx
+++ drafts/test2.cxx
@@ -4,6 +4,43 @@
 #include <vector>
 
 
+namespace color
+{
+typedef std::string s;
+
+inline s col(const std::string &x)
+{
+  return "\033[" + x + "m";
+};
+
+inline s res{col("0")};
+inline s bol{col("1")};
+inline s und{col("4")};
+inline s bli{col("5")};
+inline s inv{col("7")};
+
+inline s fblack{col("30")};
+inline s fred{col("31")};
+inline s fgreen{col("32")};
+inline s fyellow{col("33")};
+inline s fblue{col("34")};
+inline s fmagenta{col("35")};
+inline s fcyan{col("36")};
+inline s fwhite{col("37")};
+
+inline s bblack{col("40")};
+inline s bred{col("41")};
+inline s bgreen{col("42")};
+inline s byellow{col("43")};
+inline s bblue{col("44")};
+inline s bmagenta{col("45")};
+inline s bcyan{col("46")};
+inline s bwhite{col("47")};
+
+} // namespace color
+
+
+
 struct Treenode
 {
   int val{};
@@ -17,6 +54,7 @@ struct Cell
   Treenode *address{nullptr};
   std::string val{};
   bool changed{false};
+  std::string tone{};
 
   Cell(Treenode *a, std::string s, bool c) : address(a), val(s), changed(c) {};
   Cell(std::string s, bool c) : val(s), changed(c) {};
@@ -144,17 +182,23 @@ void pathToRoot(std::vector<std::vector<Cell>> &grid, Treenode *target)
     size_t lastCol = grid[row].size() - 1;
     if (target == grid[row][lastCol].address)
     {
+      grid[row][lastCol].tone = "green";
+      if (lastCol >= 2)
+      {
+        lastCol -= 2;
+      }
       while (lastCol >= 0)
       {
-        while (row > 0 and(grid[row][lastCol].val == "├─" or grid[row][lastCol].val == "│" or grid[row][lastCol].val == "└─"))
+        while (row > 0 and (grid[row][lastCol].val == "│" or grid[row][lastCol].val == "└─" or grid[row][lastCol].val == "├─"))
         {
+          grid[row][lastCol].tone = "red";
+          grid[row][lastCol + 1].tone = "red";
           --row;
         }
-        if (grid[row][lastCol].val.rfind("🔸", 0) == 0)
+        if (grid[row][lastCol].tone == "")
         {
-          break;
+          grid[row][lastCol].tone = "magenta";
         }
-        grid[row][lastCol].val = "🔸" + grid[row][lastCol].val;
 
         if (lastCol < 2)
           break;
@@ -180,7 +224,30 @@ void printGrid(std::vector<std::vector<Cell>> &grid)
       }
       else
       {
-        std::cout << n.val;
+        if (n.tone == "")
+        {
+          std::cout <<n.val;
+        }
+        else if (n.tone == "red")
+        {
+          std::cout <<color::fred <<n.val <<color::res;
+        }
+        else if (n.tone == "green")
+        {
+          std::cout <<color::fgreen <<n.val <<color::res;
+        }
+        else if (n.tone == "cyan")
+        {
+          std::cout <<color::fcyan <<n.val <<color::res;
+        }
+        else if (n.tone == "blue")
+        {
+          std::cout <<color::fblue <<n.val <<color::res;
+        }
+        else if (n.tone == "magenta")
+        {
+          std::cout <<color::fmagenta <<n.val <<color::res;
+        }
       }
     }
     std::cout << "\n";
@@ -210,89 +277,148 @@ void showTree(Treenode *root, std::vector<Treenode *> targets)
 
 int main()
 {
-  Treenode *node1 = new Treenode(1);
-  Treenode *node2 = new Treenode(2);
-  Treenode *node3 = new Treenode(3);
-  Treenode *node4 = new Treenode(4);
-  Treenode *node5 = new Treenode(5);
-  Treenode *node6 = new Treenode(6);
-  Treenode *node7 = new Treenode(7);
-  Treenode *node8 = new Treenode(8);
-  Treenode *node9 = new Treenode(9);
-  Treenode *node10 = new Treenode(10);
-  Treenode *node11 = new Treenode(11);
-  Treenode *node12 = new Treenode(12);
-  Treenode *node13 = new Treenode(13);
-  Treenode *node14 = new Treenode(14);
-  Treenode *node15 = new Treenode(15);
-  Treenode *node16 = new Treenode(16);
-  Treenode *node17 = new Treenode(17);
-  Treenode *node18 = new Treenode(18);
-  Treenode *node19 = new Treenode(19);
-  Treenode *node20 = new Treenode(20);
-  Treenode *node21 = new Treenode(21);
-  // Treenode *node22 = new Treenode(22);
-  // Treenode *node23 = new Treenode(23);
-  // Treenode *node24 = new Treenode(24);
-  Treenode *node25 = new Treenode(25);
-  // Treenode *node26 = new Treenode(26);
-  // Treenode *node27 = new Treenode(27);
-  // Treenode *node28 = new Treenode(28);
-  // Treenode *node29 = new Treenode(29);
-  Treenode *node30 = new Treenode(30);
-
-  node1->children = {node2, node3, node20};
-
-  node2->children = {node4, node5};
-
-  node4->children = {node6, node7};
-
-  node5->children = {node8, node9, node19};
-
-  node7->children = {node10, node11};
-
-  node10->children = {node12, node13};
-
-  node10->children = {node12};
-  node12->children = {node13};
-  node13->children = {node14};
-
-  node20->children = {node15, node16, node17};
-  node16->children = {node18, node30, node21};
-
-  node8->children = {node25};
-
-  // node1->children = {node2};
-  // node2->children = {node3};
-  // node3->children = {node4};
-  // node4->children = {node5};
-  // node5->children = {node6};
-  // node6->children = {node7};
-  // node7->children = {node8};
-  // node8->children = {node9};
-  // node9->children = {node10};
-  // node10->children = {node11};
-  // node11->children = {node12};
+  // Treenode *node1 = new Treenode(1);
+  // Treenode *node2 = new Treenode(2);
+  // Treenode *node3 = new Treenode(3);
+  // Treenode *node4 = new Treenode(4);
+  // Treenode *node5 = new Treenode(5);
+  // Treenode *node6 = new Treenode(6);
+  // Treenode *node7 = new Treenode(7);
+  // Treenode *node8 = new Treenode(8);
+  // Treenode *node9 = new Treenode(9);
+  // Treenode *node10 = new Treenode(10);
+  // Treenode *node11 = new Treenode(11);
+  // Treenode *node12 = new Treenode(12);
+  // Treenode *node13 = new Treenode(13);
+  // Treenode *node14 = new Treenode(14);
+  // Treenode *node15 = new Treenode(15);
+  // Treenode *node16 = new Treenode(16);
+  // Treenode *node17 = new Treenode(17);
+  // Treenode *node18 = new Treenode(18);
+  // Treenode *node19 = new Treenode(19);
+  // Treenode *node20 = new Treenode(20);
+  // Treenode *node21 = new Treenode(21);
+  // // Treenode *node22 = new Treenode(22);
+  // // Treenode *node23 = new Treenode(23);
+  // // Treenode *node24 = new Treenode(24);
+  // Treenode *node25 = new Treenode(25);
+  // // Treenode *node26 = new Treenode(26);
+  // // Treenode *node27 = new Treenode(27);
+  // // Treenode *node28 = new Treenode(28);
+  // // Treenode *node29 = new Treenode(29);
+  // Treenode *node30 = new Treenode(30);
+  //
+  // node1->children = {node2, node3, node20};
+  //
+  // node2->children = {node4, node5};
+  //
+  // node4->children = {node6, node7};
+  //
+  // node5->children = {node8, node9, node19};
+  //
+  // node7->children = {node10, node11};
+  //
+  // node10->children = {node12, node13};
+  //
+  // node10->children = {node12};
   // node12->children = {node13};
   // node13->children = {node14};
-  // node14->children = {node15};
-  // node15->children = {node16};
-  // node16->children = {node17};
-  // node17->children = {node18, node30};
-  // node18->children = {node19};
-  // node19->children = {node20};
-  // node20->children = {node21};
-  // node21->children = {node22};
-  // node22->children = {node23};
-  // node23->children = {node24};
-  // node24->children = {node25};
-  // node25->children = {node26};
-  // node26->children = {node27};
-  // node27->children = {node28};
-  // node28->children = {node29};
-  // node29->children = {node30};
   //
+  // node20->children = {node15, node16, node17};
+  // node16->children = {node18, node30, node21};
   //
-  showTree(node4, {node13, node11, node25, node21});
+  // node8->children = {node25};
+
+
+  // Create 60 nodes
+  Treenode *n1 = new Treenode(1);
+  Treenode *n2 = new Treenode(2);
+  Treenode *n3 = new Treenode(3);
+  Treenode *n4 = new Treenode(4);
+  Treenode *n5 = new Treenode(5);
+  Treenode *n6 = new Treenode(6);
+  Treenode *n7 = new Treenode(7);
+  Treenode *n8 = new Treenode(8);
+  Treenode *n9 = new Treenode(9);
+  Treenode *n10 = new Treenode(10);
+  Treenode *n11 = new Treenode(11);
+  Treenode *n12 = new Treenode(12);
+  Treenode *n13 = new Treenode(13);
+  Treenode *n14 = new Treenode(14);
+  Treenode *n15 = new Treenode(15);
+  Treenode *n16 = new Treenode(16);
+  Treenode *n17 = new Treenode(17);
+  Treenode *n18 = new Treenode(18);
+  Treenode *n19 = new Treenode(19);
+  Treenode *n20 = new Treenode(20);
+  Treenode *n21 = new Treenode(21);
+  Treenode *n22 = new Treenode(22);
+  Treenode *n23 = new Treenode(23);
+  Treenode *n24 = new Treenode(24);
+  Treenode *n25 = new Treenode(25);
+  Treenode *n26 = new Treenode(26);
+  Treenode *n27 = new Treenode(27);
+  Treenode *n28 = new Treenode(28);
+  Treenode *n29 = new Treenode(29);
+  Treenode *n30 = new Treenode(30);
+  Treenode *n31 = new Treenode(31);
+  Treenode *n32 = new Treenode(32);
+  Treenode *n33 = new Treenode(33);
+  Treenode *n34 = new Treenode(34);
+  Treenode *n35 = new Treenode(35);
+  Treenode *n36 = new Treenode(36);
+  Treenode *n37 = new Treenode(37);
+  Treenode *n38 = new Treenode(38);
+  Treenode *n39 = new Treenode(39);
+  Treenode *n40 = new Treenode(40);
+  Treenode *n41 = new Treenode(41);
+  Treenode *n42 = new Treenode(42);
+  Treenode *n43 = new Treenode(43);
+  Treenode *n44 = new Treenode(44);
+  Treenode *n45 = new Treenode(45);
+  Treenode *n46 = new Treenode(46);
+  Treenode *n47 = new Treenode(47);
+  Treenode *n48 = new Treenode(48);
+  Treenode *n49 = new Treenode(49);
+  Treenode *n50 = new Treenode(50);
+  Treenode *n51 = new Treenode(51);
+  Treenode *n52 = new Treenode(52);
+  Treenode *n53 = new Treenode(53);
+  Treenode *n54 = new Treenode(54);
+  Treenode *n55 = new Treenode(55);
+  Treenode *n56 = new Treenode(56);
+  Treenode *n57 = new Treenode(57);
+  Treenode *n58 = new Treenode(58);
+  Treenode *n59 = new Treenode(59);
+  Treenode *n60 = new Treenode(60);
+
+  // Root spread
+  n1->children = {n2, n3};
+
+  // ===== ULTRA LONG LEFT CHAIN =====
+  n2->children = {n6};
+  n6->children = {n7};
+  n7->children = {n8};
+  n8->children = {n9};
+  n9->children = {n10};
+  n10->children = {n11};
+  n11->children = {n12};
+  n12->children = {n13};
+  n13->children = {n14};
+
+  // Explosion
+  n14->children = {n15, n16, n17};
+
+  n15->children = {n18};
+  n18->children = {n19};
+  n19->children = {n20};
+
+  n16->children = {n21, n22};
+  n21->children = {n23};
+  n23->children = {n24};
+
+  n22->children = {n25};
+
+  showTree(n1, {n13, n11, n25, n21});
 
 }
